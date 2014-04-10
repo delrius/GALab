f392f18e20d1add8565d90b22ecd4aa49e6d3ce2
delrius <delrius.ua@gmail.com>
2014-04-10 01:08:58 -0400

    init

---
+++ src/ga/lab/Encoding.java
@@ -0,0 +1,33 @@
+package ga.lab;
+
+import java.util.HashMap;
+
+public class Encoding {
+
+	// static int par1 = 10;
+
+	public static HashMap<Integer, String> encode() {
+		HashMap<Integer, String> encodeResult = new HashMap<Integer, String>();
+		for (int i = 1; i <= 1000; i++) {
+			String key;
+			if (Integer.toBinaryString(i).toString().length() == 10) {
+				key = Integer.toBinaryString(i).toString();
+			} else {
+				String temp1 = "0000000000" + Integer.toBinaryString(i).toString();
+				key = (temp1).substring(temp1.length() - 10, temp1.length());
+			}
+
+			encodeResult.put(i, key);
+		}
+		return encodeResult;
+	}
+
+	public static void main(String[] args) {
+
+		HashMap<Integer, String> res = encode();
+		System.out.println(res.get(55));
+		System.out.println(res.get(1000));
+		System.out.println(Operators.getHammingDistance(res.get(55), res.get(1000)));
+
+	}
+}
---
+++ src/ga/lab/Operators.java
@@ -0,0 +1,103 @@
+package ga.lab;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Operators {
+
+	// Відстань Хемінга
+
+	public static int getHammingDistance(String ch1, String ch2) {
+		if (ch1.length() != ch2.length()) {
+			System.out.println("The length of two chromosomes don't match");
+		}
+		int distance = 0;
+		for (int i = 0; i < ch1.length(); i++) {
+			if (ch1.charAt(i) != ch2.charAt(i))
+				distance++;
+		}
+		return distance;
+	}
+
+	// Евклідова відстань
+
+	// Функція співучасті
+
+	public static double sharingFunction(String ch1, String ch2, int radius, int alpha) {
+		double sharingFunction;
+		if (getHammingDistance(ch1, ch2) < radius) {
+			sharingFunction = (1 - (Math.pow((getHammingDistance(ch1, ch2) / radius), alpha)));
+		} else {
+			sharingFunction = 0;
+		}
+		return sharingFunction;
+	}
+
+	// Радіус ніші
+
+	public static double calculateRadius(int dimension, int numberOfExtr, double r) {
+		// я перетворила корінь степеня D в таке, бо не знайшлакорінь певного
+		// степеня в бібліотеці
+		return r / Math.sqrt(Math.pow(numberOfExtr, 1 / dimension));
+	}
+
+	// допоміжна функція r (поки не вшарила як її рахувати, додаю статтю де на 5 сторінці вона є 2.5 The Niche Radius Problem)
+	public static double calculateR(int dimension, int numberOfExtr, double r) {
+		return 0;
+	}
+
+	// Число ніші
+
+	public static double calculateNicheNumber(String ch1, ArrayList<String> selectedChList, int N, int radius, int alpha) {
+		double m = 0;
+		for (int i = 0; i < N; i++) {
+			m = m + sharingFunction(ch1, selectedChList.get(i), radius, alpha);
+		}
+		return m;
+	}
+
+	// оцінка - функція здоров'я
+	// хм! не знаю як другим параметром передавати функцію відповідну %) тому
+	// поки для мене вихід описати окремо для всіх функцій F15, F16, F18 та F19
+	public static double fitnessFunctionF15(int n, double x) {
+		double fitness = 0;
+		for (int i = 0; i < n; i++) {
+			fitness = Math.pow(Math.sin(5 * Math.PI * x), 6);
+		}
+		return fitness = 1 / n * fitness;
+	}
+
+	//думаю що теж варто наперед мати таблицб обраховної функції здоров'я для всіх хромосом? Що скажеш?
+	public static HashMap<Integer, Double> calcInitialFitnessMap(int n, String funcName) {
+		HashMap<Integer, Double> initialFitnessMap = new HashMap<Integer, Double>();
+		for (int i = 1; i <= 1000; i++) {
+			if (funcName.equals("F15")) {
+				initialFitnessMap.put(i, fitnessFunctionF15(n, i / 1000));
+			} else if (funcName.equals("F16")) { // to do
+			} else if (funcName.equals("F18")) { // to do
+			} else if (funcName.equals("F19")) { // to do
+			}
+
+		}
+		return initialFitnessMap;
+	}
+	
+	//Пониження функції пристосованості в ніші
+	
+	//Кросинговер
+	
+	//Відбір
+	
+	//Мутація
+	
+	
+	
+	// відбір
+	// public static ArrayList<String> selection (int dimension)
+	// {
+	// ArrayList<String> selectedChromosomes = new ArrayList<String>();
+	// return selectedChromosomes;
+	//
+	// }
+
+}
