236e300fc3cb42c6462bb1d9f078f2c152caf43d
delrius <skylady.mary@gmail.com>
2014-04-15 23:18:23 +0300

    roulete selection

---
+++ src/ga/lab/Individual.java
@@ -0,0 +1,14 @@
+package ga.lab;
+
+public class Individual {
+    private double fitness;
+
+
+    public double getFitness() {
+        return fitness;
+    }
+
+    public void setFitness(double fitness) {
+        this.fitness = fitness;
+    }
+}
---
+++ src/ga/lab/stages/ISelection.java
@@ -0,0 +1,9 @@
+package ga.lab.stages;
+
+import ga.lab.Individual;
+
+import java.util.List;
+
+public interface ISelection {
+    List<Individual> performSelection(List<Individual> population, int popSizee);
+}
