d2c19ef2627d01596a0ddcf78bdf1eb2e6d3c8bd
delrius <skylady.mary@gmail.com>
2014-04-15 22:34:16 +0300

    refactor

--- src/ga/lab/functions/Functions.java
+++ src/ga/lab/functions/Functions.java
@@ -3,7 +3,7 @@ package ga.lab.functions;
 import java.util.List;
 
 public enum Functions implements IFunction {
-    F15(new F15());
+    F15(new F15()), F16(new F16()), F18(new F18()), F19(new F19());
 
     Functions(IFunction function) {
         this.function = function;
