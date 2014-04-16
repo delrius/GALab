3645b1af8366d5c6be0710c678d747330bfe9c9c
sktylady <skylady.mary@gmail.com>
2014-04-16 10:24:01 +0300

    add tests for radius and Hamming distance calculation

--- test/ga/lab/OperatorsTest.java
+++ test/ga/lab/OperatorsTest.java
@@ -1,12 +1,36 @@
 package ga.lab;
 
+import ga.lab.Chromosome;
+import ga.lab.Operators;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class OperatorsTest {
-    @Test
-    public void testCalculateR() throws Exception {
-        double r = Operators.calculateR(3, 0, 1);
-        Assert.assertEquals("R is calculated wrong", r, 1./2 * Math.sqrt(3), 0.001);
-    }
+	@Test
+	public void testCalculateR() throws Exception {
+		double r = Operators.calculateR(3, 0, 1);
+		Assert.assertEquals("R is calculated wrong", r, 1. / 2 * Math.sqrt(3),
+				0.001);
+	}
+
+	@Test
+	public void testHammingDistance() throws Exception {
+		int n1 = 1;
+		int n2 = 3;
+		Chromosome chr1 = new Chromosome(n1);
+		Chromosome chr2 = new Chromosome(n2);
+		int expected = 2;
+		double actual = Operators.getHammingDistance(chr1, chr2);
+		Assert.assertEquals("distance =" + actual, expected, actual, 0);
+	}
+	@Test
+	public void testCalculateRadius() {
+		int n = 1;
+		int numberOfExtr = (int) Math.pow(5, n);
+		double r = Operators.calculateR(n, 0, 1);
+		double actual = Operators.calculateRadius(n, numberOfExtr, r);
+		double expected = 0.1;
+		Assert.assertEquals("radius =" + actual, expected, actual, 0.01);
+	}
 }
